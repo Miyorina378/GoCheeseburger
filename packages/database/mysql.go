26e04e90684378302aa4a13fa917e67337a1f091
Miyorina378 <darendianana@gmail.com>
2025-07-03 12:25:42 +0700

    V1.0

--- packages/database/mysql.go
+++ packages/database/mysql.go
@@ -1,1 +1,83 @@
+package database
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+func NewMySqlConnection() (*sql.DB, error) {
+
+	b2c := connMySqlB2C()
+	conn, err := connectMySQL(b2c)
+	if err != nil {
+		return nil, err
+	}
+
+	setupConnectionPool(conn)
+	return conn, nil
+}
+
+func NewMySqlBcloudMS() (*sql.DB, error) {
+
+	ms := connMySqlBcloudMS()
+	conn, err := connectMySQL(ms)
+	if err != nil {
+		return nil, err
+	}
+
+	setupConnectionPool(conn)
+	return conn, nil
+}
+
+func connectMySQL(config mysql.Config) (*sql.DB, error) {
+
+	conn, err := sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+	if err := conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func connMySqlB2C() mysql.Config {
+
+	config := mysql.Config{
+		User:                 "censored",
+		Passwd:               "censored",
+		Net:                  "tcp",
+		Addr:                 "amazonaws.com",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+		DBName:               "miyorin",
+	}
+
+	return config
+}
+
+func connMySqlBcloudMS() mysql.Config {
+
+	config := mysql.Config{
+		User:                 "censored",
+		Passwd:               "censored",
+		Net:                  "tcp",
+		Addr:                 "amazonaws.com",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+		DBName:               "miyorin",
+	}
+
+	return config
+}
+
+func setupConnectionPool(db *sql.DB) {
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(time.Hour)
+	db.SetConnMaxIdleTime(30 * time.Minute)
+}
 
